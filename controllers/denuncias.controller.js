114a04919afe0dbe85569d84f79b0916812f2f7e
equipoNO8 <128776459+equipoNO8@users.noreply.github.com>
2023-03-26 16:18:55 +0000

    request-params updated

--- controllers/denuncias.controller.js
+++ controllers/denuncias.controller.js
@@ -1,11 +1,16 @@
 const mysqlclient = require("../helpers/dbConnectionClient")
 
+const uniqueInteger = () => {
+    const prefix = new Date().getTime().toString(); // Obtener la fecha actual en milisegundos
+    const randomNumber = Math.floor(Math.random() * 1000000); // Generar un número aleatorio de 0 a 999999
+    return parseInt(prefix + randomNumber); // Concatenar el prefijo y el número aleatorio, y convertirlo en un entero
+  }
+
 function post(req, res){
     const {
         lugar,
         denuncia,
-        fecha, 
-        hora, 
+        folio = uniqueInteger(), 
         correo, 
         edad
     } = req.body;
@@ -16,31 +21,22 @@ function post(req, res){
             if(error){
                 //
                 console.log("Connection error")
-                console.log(error)
                 return res.status(500).json({
                     message: "INTERNAL SERVER ERROR"
                 })
             }
         });
-
-        // mysqlclient.query( `INSERT INTO Denuncia (lugar, denuncia, fecha, hora, correo, edad) VALUES (${lugar}, ${denuncia}, ${fecha}, ${hora}, ${correo}, ${edad};`, function(error, results, fields){
-        //     if(error)
-        //     {
-        //         return res.status(400).json({
-        //             message: "bad request"
-        //         })
-        //     }
-        // })
-        mysqlclient.query( `SHOW * FROM denuncias;`, function(error, results, fields){
+        const fecha = new Date().toISOString().slice(0, 10);
+        const hora = new Date().toISOString().slice(11, 19);
+        mysqlclient.query( `INSERT INTO Denuncia (fecha, hora, denuncia, Correo, edad, Folios, lugar) VALUES (${fecha}, ${hora}, ${denuncia}, ${correo}, ${edad}, ${folio}, ${lugar};`, function(error){
             if(error)
             {
                 return res.status(400).json({
                     message: "bad request"
                 })
             }
-            console.log(results)
-            console.log(fields)
         })
+        
     }
     catch(error)
     {
